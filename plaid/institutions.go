eb0213ff1349ca92d59ea3424b6c16cc6206f593
Tan <tan@plaid.com>
2019-05-28 13:41:25 -0700

    use pointers for nullable

--- plaid/institutions.go
+++ plaid/institutions.go
@@ -16,14 +16,14 @@ type Institution struct {
 	CountryCodes []string     `json:"country_codes"`
 
 	// Included when `options.include_status` is true.
-	InstitutionStatus InstitutionStatus `json:"status"`
+	InstitutionStatus *InstitutionStatus `json:"status",omitempty`
 
+	// May be included when `options.include_optional_metadata` is true.
+	PrimaryColor *string `json:"primary_color",omitempty`
 	// Included when `options.include_optional_metadata` is true.
-	PrimaryColor string `json:"primary_color"`
-	// Included when `options.include_optional_metadata` is true.
-	URL string `json:"url"`
-	// Included when `options.include_optional_metadata` is true.
-	Logo string `json:"logo"`
+	URL *string `json:"url",omitempty`
+	// May be included when `options.include_optional_metadata` is true.
+	Logo *string `json:"logo",omitempty`
 }
 
 type InstitutionStatus struct {
