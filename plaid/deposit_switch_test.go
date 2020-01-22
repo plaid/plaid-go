17c6efa9c6247eb523d0d68af0c54431b0dd0f9c
jeffzwang <jeffreyzhiyuanwang@gmail.com>
2020-01-21 16:50:09 -0800

    add deposit switch create test

--- plaid/deposit_switch_test.go
+++ plaid/deposit_switch_test.go
@@ -1,12 +1,27 @@
 package plaid
 
 import (
-// "testing"
-// assert "github.com/stretchr/testify/require"
+	"strings"
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
 )
 
-// func TestCreateDepositSwitch(t *testing.T) {
-// 	sandboxResp, _ := testClient.CreateSandboxPublicToken(sandboxInstitution, []string{"deposit_switch"})
-// 	tokenResp, _ := testClient.ExchangePublicToken(sandboxResp.PublicToken)
-// 	depositSwitchResp, err := testClient.CreateDepositSwitch(tokenResp.AccessToken)
-// }
+func TestCreateDepositSwitch(t *testing.T) {
+	accessTokenResp, err := testClient.ImportItem([]string{"identity", "auth"}, map[string]interface{}{
+		"user_id":    "user_good",
+		"auth_token": "pass_good",
+	}, importItemRequestOptions{
+		Webhook: "https://plaid.com/webhook-test",
+	})
+	assert.Nil(t, err)
+	assert.NotNil(t, accessTokenResp)
+	assert.True(t, strings.HasPrefix(accessTokenResp.AccessToken, "access-sandbox"))
+
+	getAccountsResp, err := testClient.GetAccounts(accessTokenResp.AccessToken)
+	assert.Nil(t, err)
+	targetAccountID := getAccountsResp.Accounts[0].AccountID
+	depositSwitchResp, err := testClient.CreateDepositSwitch(targetAccountID, accessTokenResp.AccessToken)
+	assert.Nil(t, err)
+	assert.True(t, depositSwitchResp.DepositSwitchID != "")
+}
