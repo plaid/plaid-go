82ea2f291a5bb790058dcee3ac15cb1acb3bdca5
jeffzwang <jeffreyzhiyuanwang@gmail.com>
2020-01-21 16:35:10 -0800

    add deposit switch route

---
+++ plaid/deposit_switch.go
@@ -0,0 +1,114 @@
+package plaid
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+type getDepositSwitchResponse struct {
+	DepositSwitchID               string  `json:"deposit_switch_id"`
+	TargetItemID                  string  `json:"target_item_id"`
+	TargetAccountID               string  `json:"target_account_id"`
+	State                         string  `json:"state"`
+	RequestID                     string  `json:"request_id"`
+	CreatedDate                   string  `json:"created_date,omitempty"`
+	IsAllocatedRemainder          bool    `json:"is_allocated_remainder,omitempty"`
+	AccountHasMultipleAllocations bool    `json:"account_has_multiple_allocations,omitempty"`
+	PercentAllocated              float64 `json:"percent_allocated,omitempty"`
+	CompletedDate                 string  `json:"completed_date,omitempty"`
+}
+
+type getDepositSwitchRequest struct {
+	ClientID        string `json:"client_id"`
+	Secret          string `json:"secret"`
+	DepositSwitchID string `json:"deposit_switch_id"`
+}
+
+// GetDepositSwitch retrieves deposit switch data.
+func (c *Client) GetDepositSwitch(
+	depositSwitchID string,
+) (resp getDepositSwitchResponse, err error) {
+	if depositSwitchID == "" {
+		return resp, errors.New("/deposit_switch/get - deposit switch id must be specified")
+	}
+	req := getDepositSwitchRequest{
+		ClientID:        c.clientID,
+		Secret:          c.secret,
+		DepositSwitchID: depositSwitchID,
+	}
+	jsonBody, err := json.Marshal(req)
+	if err != nil {
+		return resp, err
+	}
+
+	err = c.Call("/deposit_switch/get", jsonBody, &resp)
+
+	return getDepositSwitchResponse{}, err
+}
+
+// this is exactly the same as getDepositSwitchRequest
+type createDepositSwitchTokenRequest struct {
+	ClientID        string `json:"client_id"`
+	Secret          string `json:"secret"`
+	DepositSwitchID string `json:"deposit_switch_id"`
+}
+
+type createDepositSwitchTokenResponse struct {
+	DepositSwitchToken string `json:"deposit_switch_token"`
+	Expires            string `json:"deposit_switch_token_expiration_time"`
+}
+
+func (c *Client) CreateDepositSwitchToken(
+	depositSwitchID string,
+) (resp createDepositSwitchTokenResponse, err error) {
+	if depositSwitchID == "" {
+		return resp, errors.New("/deposit_switch/token/create - deposit switch id must be specified")
+	}
+	req := createDepositSwitchTokenRequest{
+		ClientID:        c.clientID,
+		Secret:          c.secret,
+		DepositSwitchID: depositSwitchID,
+	}
+	jsonBody, err := json.Marshal(req)
+	if err != nil {
+		return resp, err
+	}
+
+	err = c.Call("/deposit_switch/token/create", jsonBody, &resp)
+
+	return resp, err
+}
+
+type createDepositSwitchRequest struct {
+	ClientID          string `json:"client_id"`
+	Secret            string `json:"secret"`
+	TargetAccountID   string `json:"target_account_id"`
+	TargetAccessToken string `json:"target_access_token"`
+}
+
+type createDepositSwitchResponse struct {
+	DepositSwitchID string `json:"deposit_switch_id"`
+}
+
+func (c *Client) CreateDepositSwitch(targetAccountID string, targetAccessToken string) (resp createDepositSwitchResponse, err error) {
+	if targetAccountID == "" {
+		return resp, errors.New("/deposit_switch/create - target account id must be specified")
+	}
+	if targetAccessToken == "" {
+		return resp, errors.New("/deposit_switch/create - target access token must be specified")
+	}
+	req := createDepositSwitchRequest{
+		ClientID:          c.clientID,
+		Secret:            c.secret,
+		TargetAccountID:   targetAccountID,
+		TargetAccessToken: targetAccessToken,
+	}
+	jsonBody, err := json.Marshal(req)
+	if err != nil {
+		return resp, err
+	}
+
+	err = c.Call("/deposit_switch/create", jsonBody, &resp)
+
+	return resp, err
+}
---
+++ plaid/deposit_switch_test.go
@@ -0,0 +1,12 @@
+package plaid
+
+import (
+// "testing"
+// assert "github.com/stretchr/testify/require"
+)
+
+// func TestCreateDepositSwitch(t *testing.T) {
+// 	sandboxResp, _ := testClient.CreateSandboxPublicToken(sandboxInstitution, []string{"deposit_switch"})
+// 	tokenResp, _ := testClient.ExchangePublicToken(sandboxResp.PublicToken)
+// 	depositSwitchResp, err := testClient.CreateDepositSwitch(tokenResp.AccessToken)
+// }
