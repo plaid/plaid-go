c3e5c27f33288ecb589db8be8af8f3e610f8fc36
Andrew Chen <andrewchen@plaid.com>
2020-07-07 21:21:30 -0700

    fix bugs

--- plaid/link_token_test.go
+++ plaid/link_token_test.go
@@ -10,9 +10,11 @@ import (
 
 func TestCreateLinkTokenRequired(t *testing.T) {
 	linkTokenResp, err := testClient.CreateLinkToken(LinkTokenConfigs{
-		ClientUserID: time.Now().String(),
-		ClientName:   "Plaid Test",
-		Products:     []string{"auth"},
+		User: LinkTokenUser{
+			ClientUserID: time.Now().String(),
+		},
+		ClientName: "Plaid Test",
+		Products:   []string{"auth"},
 	})
 
 	assert.Nil(t, err)
@@ -22,14 +24,21 @@ func TestCreateLinkTokenRequired(t *testing.T) {
 
 func TestCreateLinkTokenOptional(t *testing.T) {
 	linkTokenResp, err := testClient.CreateLinkToken(LinkTokenConfigs{
-		ClientUserID:          time.Now().String(),
+		User: LinkTokenUser{
+			ClientUserID:             time.Now().String(),
+			LegalName:                "Legal Name",
+			PhoneNumber:              "1234567890",
+			EmailAddress:             "test@email.com",
+			PhoneNumberVerifiedTime:  time.Now(),
+			EmailAddressVerifiedTime: time.Now(),
+		},
 		ClientName:            "Plaid Test",
 		Products:              []string{"auth"},
 		CountryCodes:          []string{"US"},
 		Language:              "en",
 		Webhook:               "https://webhook-uri.com",
 		LinkCustomizationName: "default",
-		AccountFilters: map[string]map[string][]string{
+		AccountFilters: &map[string]map[string][]string{
 			"depository": map[string][]string{
 				account_subtypes: []string{"checking", "savings"},
 			},
