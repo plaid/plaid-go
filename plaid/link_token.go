f3cad1af34ef13d3eade1842b552b120d51647c2
Andrew Chen <andrewchen@plaid.com>
2020-07-07 21:10:56 -0700

    add link/token/create support

---
+++ plaid/link_token.go
@@ -0,0 +1,68 @@
+package plaid
+
+import (
+	"encoding/json"
+	"errors"
+	"time"
+)
+
+type LinkTokenUser struct {
+	ClientUserID             string    `json:"client_user_id"`
+	LegalName                string    `json:"legal_name,omitempty"`
+	PhoneNumber              string    `json:"phone_number,omitempty"`
+	EmailAddress             string    `json:"email_address,omitempty"`
+	PhoneNumberVerifiedTime  time.Time `json:"phone_number_verified_time,omitempty"`
+	EmailAddressVerifiedTime time.Time `json:"email_address_verified_time,omitempty"`
+}
+
+type CrossAppItemAdd struct {
+	TargetApplicationToken string `json:"target_application_token"`
+	ForeignID              string `json:"foreign_id,omitempty"`
+}
+
+type PaymentInitiation struct {
+	PaymentID string `json:"payment_id"`
+}
+
+type LinkTokenConfigs struct {
+	User                  LinkTokenUser                   `json:"user"`
+	ClientName            string                          `json:"client_name"`
+	Products              []string                        `json:"products,omitempty"`
+	AccessToken           string                          `json:"access_token,omitempty"`
+	CountryCodes          []string                        `json:"country_codes,omitempty"`
+	Webhook               string                          `json:"webhook,omitempty"`
+	AccountFilters        *map[string]map[string][]string `json:"account_filters,omitempty"`
+	CrossAppItemAdd       CrossAppItemAdd                 `json:"cross_app_item_add,omitempty"`
+	PaymentInitiation     PaymentInitiation               `json:"payment_initiation,omitempty"`
+	Language              string                          `json:"language,omitempty"`
+	Locale                string                          `json:"locale,omitempty"`
+	LinkCustomizationName string                          `json:"link_customization_name,omitempty"`
+	RedirectUri           string                          `json:"redirect_uri,omitempty"`
+	InstitutionId         string                          `json:"institution_id,omitempty"`
+}
+
+type createLinkTokenRequest struct {
+	ClientID string `json:"client_id"`
+	Secret   string `json:"secret"`
+	LinkTokenConfigs
+}
+
+type CreateLinkTokenResponse struct {
+	Token      string    `json:"token"`
+	Expiration time.Time `json:"expiration"`
+}
+
+func (c *Client) CreateLinkToken(configs LinkTokenConfigs) (resp CreateLinkTokenResponse, err error) {
+	jsonBody, err := json.Marshal(createLinkTokenRequest{
+		ClientID:         c.clientID,
+		Secret:           c.secret,
+		LinkTokenConfigs: configs,
+	})
+
+	if err != nil {
+		return resp, err
+	}
+
+	err = c.Call("", jsonBody, &resp)
+	return resp, err
+}
---
+++ plaid/link_token_test.go
@@ -0,0 +1,42 @@
+package plaid
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	assert "github.com/stretchr/testify/require"
+)
+
+func TestCreateLinkTokenRequired(t *testing.T) {
+	linkTokenResp, err := testClient.CreateLinkToken(LinkTokenConfigs{
+		ClientUserID: time.Now().String(),
+		ClientName:   "Plaid Test",
+		Products:     []string{"auth"},
+	})
+
+	assert.Nil(t, err)
+	assert.True(t, strings.HasPrefix(linkTokenResp.Token, "link-sandbox"))
+	assert.NotZero(t, linkTokenResp.Expiration)
+}
+
+func TestCreateLinkTokenOptional(t *testing.T) {
+	linkTokenResp, err := testClient.CreateLinkToken(LinkTokenConfigs{
+		ClientUserID: time.Now().String(),
+		ClientName:   "Plaid Test",
+		Products:     []string{"auth"},
+		CountryCodes: []string{"US"},
+		Language:     "en",
+		Webhook:      "https://webhook-uri.com",
+		LinkCustomizationName: "default",
+		AccountFilters: map[string]map[string][]string{
+			"depository": map[string][]string{
+				account_subtypes: []string{"checking", "savings"},
+			}
+		},
+	})
+
+	assert.Nil(t, err)
+	assert.True(t, strings.HasPrefix(linkTokenResp.Token, "link-sandbox"))
+	assert.NotZero(t, linkTokenResp.Expiration)
+}
