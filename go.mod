c3eb2c5373cc5f28ae6eda70fdc2d367a7b0734f
Stephen Jayakar <aj12ay@gmail.com>
2021-08-02 16:15:39 -0700

    v0.1.0-beta.1 (#182)

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/plaid/plaid-go
+
+go 1.13
+
+require (
+	github.com/stretchr/testify v1.4.0
+	golang.org/x/oauth2 v0.0.0-20210218202405-ba52d332ba99
+)
--- internal/internal.go
+++
@@ -1,3 +0,0 @@
-// Package internal holds logic internal to the plaid-go implementation and
-// should not be imported from outside of the plaid-go package.
-package internal
