ab4e278a191c084cb375e0edbffac161f5d98b70
Stephen Jayakar <sjayakar@plaid.com>
2022-02-03 12:34:28 -0800

    OpenAPI generated code at 2022-02-03T20:34:22Z

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/plaid/plaid-go
+module github.com/plaid/plaid-go/v2
 
 go 1.13
 
--- tests/bank_transfer_test.go
+++ tests/bank_transfer_test.go
@@ -6,7 +6,7 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/plaid/plaid-go/plaid"
+	"github.com/plaid/plaid-go/v2/plaid"
 	"github.com/stretchr/testify/assert"
 )
 
