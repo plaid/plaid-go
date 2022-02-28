41df5a3192d139a4912dec271667f63d257eb034
Li Zhang <li3zhang@gmail.com>
2022-02-28 17:45:33 -0500

    OpenAPI generated code at 2022-02-28T19:56:47Z (#228)
    Co-authored-by: Li Zhang <lzhang@plaid.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # Go embeds the package version in the generator.
-GO_PACKAGE_VERSION=2.0.1
+GO_PACKAGE_VERSION=2.1.0
 
 .PHONY: test
 test:
---
+++ tests/income_verification_test.go
@@ -0,0 +1,70 @@
+package tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/plaid/plaid-go/v2/plaid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPayStubsGet(t *testing.T) {
+	testClient := NewTestClient()
+	ctx := context.Background()
+	accessToken := createSandboxItem(
+		t,
+		ctx,
+		testClient,
+		"ins_129618",
+		[]plaid.Products{plaid.PRODUCTS_INCOME_VERIFICATION},
+	)
+
+	request := plaid.NewIncomeVerificationPaystubsGetRequest()
+	request.SetAccessToken(accessToken)
+
+	resp, _, err := testClient.PlaidApi.IncomeVerificationPaystubsGet(ctx).IncomeVerificationPaystubsGetRequest(*request).Execute()
+
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(resp.GetPaystubs()))
+}
+
+func TestDocumentsDownload(t *testing.T) {
+	testClient := NewTestClient()
+	ctx := context.Background()
+
+	accessToken := createSandboxItem(
+		t,
+		ctx,
+		testClient,
+		"ins_129618",
+		[]plaid.Products{plaid.PRODUCTS_INCOME_VERIFICATION},
+	)
+
+	request := plaid.NewIncomeVerificationDocumentsDownloadRequest()
+	request.SetAccessToken(accessToken)
+
+	resp, _, err := testClient.PlaidApi.IncomeVerificationDocumentsDownload(ctx).IncomeVerificationDocumentsDownloadRequest(*request).Execute()
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func TestPreCheck(t *testing.T) {
+	testClient := NewTestClient()
+	ctx := context.Background()
+
+	precheckEmployer := plaid.IncomeVerificationPrecheckEmployer{}
+	precheckEmployer.SetName("Plaid")
+
+	precheckUser := plaid.IncomeVerificationPrecheckUser{}
+	precheckUser.SetFirstName("Jane")
+	precheckUser.SetLastName("Doe")
+
+	request := plaid.NewIncomeVerificationPrecheckRequest()
+	request.SetEmployer(precheckEmployer)
+	request.SetUser(precheckUser)
+
+	resp, _, err := testClient.PlaidApi.IncomeVerificationPrecheck(ctx).IncomeVerificationPrecheckRequest(*request).Execute()
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, plaid.IncomeVerificationPrecheckConfidence("UNKNOWN"), resp.GetConfidence())
+}
