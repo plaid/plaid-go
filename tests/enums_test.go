3c5af87a74db449b98c2a4a64d888b6c1fbf91b2
James King <jking@plaid.com>
2022-11-17 09:51:59 -0800

    OpenAPI generated code at 2022-11-17T17:51:53Z

---
+++ tests/enums_test.go
@@ -0,0 +1,18 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/plaid/plaid-go/v10/plaid"
+	"github.com/stretchr/testify/assert"
+)
+
+// We want to be able to accept new enum values as they're added to the API
+// without needing clients to update their client library versions. The API
+// will error if it doesn't recognize the enum so we still have validation.
+func TestUnknownEnums(t *testing.T) {
+	val, err := plaid.NewAccountSelectionCardinalityFromValue("UNKNOWN_VALUE")
+
+	assert.NoError(t, err)
+	assert.Equal(t, *val, plaid.AccountSelectionCardinality("UNKNOWN_VALUE"))
+}
