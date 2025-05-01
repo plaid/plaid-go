071d81627c3a64f9c14db1827c9da2bfe341d7bd
lzhang <lzhang@plaid.com>
2025-05-01 15:27:04 -0400

    OpenAPI generated code at 2025-05-01T19:26:55Z

--- tests/investments_auth_test.go
+++ tests/investments_auth_test.go
@@ -2,7 +2,7 @@ package tests
 
 import (
 	"context"
-	"github.com/plaid/plaid-go/v33/plaid"
+	"github.com/plaid/plaid-go/v34/plaid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
