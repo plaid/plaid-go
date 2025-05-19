4a13ec9c034da1deeaefb2da230112cda24cbbd9
Matt Nguyen <mnguyen@plaid.com>
2025-05-19 11:52:06 -0700

    OpenAPI generated code at 2025-05-19T18:51:57Z

--- tests/item_test.go
+++ tests/item_test.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/plaid/plaid-go/v34/plaid"
+	"github.com/plaid/plaid-go/v35/plaid"
 	"github.com/stretchr/testify/assert"
 )
 
--- tests/transactions_test.go
+++ tests/transactions_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/plaid/plaid-go/v34/plaid"
+	"github.com/plaid/plaid-go/v35/plaid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
