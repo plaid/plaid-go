2d649f161ac344db00ecd82a25d33a4cd3740414
James King <jking@plaid.com>
2025-08-13 12:26:58 -0700

    OpenAPI generated code at 2025-08-13T19:26:47Z

--- tests/processor_test.go
+++ tests/processor_test.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/plaid/plaid-go/v38/plaid"
+	"github.com/plaid/plaid-go/v39/plaid"
 	assert "github.com/stretchr/testify/require"
 )
 
--- tests/sandbox_test.go
+++ tests/sandbox_test.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"testing"
 
-	"github.com/plaid/plaid-go/v38/plaid"
+	"github.com/plaid/plaid-go/v39/plaid"
 	assert "github.com/stretchr/testify/require"
 )
 
