5b30cfbbeeabd9cfb81935409a0b8d9b6aa84d40
Andy Nguyen <anguyen@plaid.com>
2026-07-22 19:38:32 -0400

    OpenAPI generated code at 2026-07-22T23:38:21Z

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/plaid/plaid-go/v43
+module github.com/plaid/plaid-go/v44
 
 go 1.13
 
--- tests/statements_test.go
+++ tests/statements_test.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/plaid/plaid-go/v43/plaid"
+	"github.com/plaid/plaid-go/v44/plaid"
 )
 
 func TestStatementsFullFlow(t *testing.T) {
